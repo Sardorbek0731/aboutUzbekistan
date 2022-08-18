4e9839f449b6b3125946dcd62150e18671ee9eaf
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-08-18 18:16:51 +0500

    added footer javascript alert

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
-const signupForm = document.getElementById("signup-form");
-const warningAlert = document.getElementById("warning_alert");
-const correctAlert = document.getElementById("correct_alert");
-const submit = document.getElementById("submit");
-const warningCloseAlert = document.getElementById("closeWarning_alert");
-const correctCloseAlert = document.getElementById("closeCorrect_alert");
-
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  let regEx = /[a-zA-Z]{2,}/;
-  let regExEmail = /[a-zA-Z0-9]{2,}@gmail/;
-  let regExPassword = /[a-zA-Z0-9]{2,}/;
-  let ism = signupForm.firstName.value;
-  let email = signupForm.email.value;
-  let password = signupForm.password.value;
-  if (
-    regEx.test(ism) &&
-    regExEmail.test(email) &&
-    regExPassword.test(password)
-  ) {
-    correctAlert.classList.add("active");
-    correctCloseAlert.classList.add("active");
-    signupForm.firstName.value = "";
-    signupForm.email.value = "";
-    signupForm.password.value = "";
-  } else {
-    warningAlert.classList.add("active");
-    warningCloseAlert.classList.add("active");
-  }
-});
-warningCloseAlert.addEventListener("click", () => {
-  warningAlert.classList.remove("active");
-  warningCloseAlert.classList.remove("active");
-});
-correctCloseAlert.addEventListener("click", () => {
-  correctAlert.classList.remove("active");
-  correctCloseAlert.classList.remove("active");
-});
+// Scroll Header
 
 const scrollHeader = document.querySelector("nav");
 
@@ -79,4 +42,75 @@ inputSearch.addEventListener("click", () => {
       message.textContent = "";
     }, 4000);
   }
+  searchInputValue.value = "";
+});
+
+// Alert progress
+const signupForm = document.getElementById("signup-form");
+const warningAlert = document.getElementById("warning_alert");
+const correctAlert = document.getElementById("correct_alert");
+const submit = document.getElementById("submit");
+const warningCloseAlert = document.getElementById("closeWarning_alert");
+const correctCloseAlert = document.getElementById("closeCorrect_alert");
+const warningProgress = document.querySelector(".warning-progress");
+const correctProgress = document.querySelector(".correct-progress");
+
+function warningAlertProgress() {
+  setTimeout(() => {
+    warningProgress.classList.remove("active");
+  }, 5400);
+}
+function correctAlertProgress() {
+  setTimeout(() => {
+    correctProgress.classList.remove("active");
+  }, 5400);
+}
+function warningCloseAlertProgress() {
+  setTimeout(() => {
+    warningProgress.classList.remove("active");
+  }, 300);
+}
+function correctCloseAlertProgress() {
+  setTimeout(() => {
+    correctProgress.classList.remove("active");
+  }, 300);
+}
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  let regEx = /[a-zA-Z]{2,}/;
+  let regExEmail = /[a-zA-Z0-9]{2,}@gmail/;
+  let regExPassword = /[a-zA-Z0-9]{2,}/;
+  let ism = signupForm.firstName.value;
+  let email = signupForm.email.value;
+  let password = signupForm.password.value;
+  if (
+    regEx.test(ism) &&
+    regExEmail.test(email) &&
+    regExPassword.test(password)
+  ) {
+    correctAlert.classList.add("active");
+    correctProgress.classList.add("active");
+    setTimeout(() => {
+      correctAlert.classList.remove("active");
+    }, 5000);
+    correctAlertProgress();
+    signupForm.firstName.value = "";
+    signupForm.email.value = "";
+    signupForm.password.value = "";
+  } else {
+    warningAlert.classList.add("active");
+    warningProgress.classList.add("active");
+    setTimeout(() => {
+      warningAlert.classList.remove("active");
+    }, 5000);
+    warningAlertProgress();
+  }
+});
+warningCloseAlert.addEventListener("click", () => {
+  warningAlert.classList.remove("active");
+  warningCloseAlertProgress();
+});
+correctCloseAlert.addEventListener("click", () => {
+  correctAlert.classList.remove("active");
+  correctCloseAlertProgress();
 });
