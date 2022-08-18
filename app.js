806cabce6d566b47b130fad0ff9f754a4628d181
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-08-18 13:31:03 +0500

    added search js ga o'zgartirdim

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ submit.addEventListener("click", (e) => {
     signupForm.firstName.value = "";
     signupForm.email.value = "";
     signupForm.password.value = "";
-    
   } else {
     warningAlert.classList.add("active");
     warningCloseAlert.classList.add("active");
@@ -36,4 +35,48 @@ warningCloseAlert.addEventListener("click", () => {
 correctCloseAlert.addEventListener("click", () => {
   correctAlert.classList.remove("active");
   correctCloseAlert.classList.remove("active");
+});
+
+const scrollHeader = document.querySelector("nav");
+
+document.addEventListener("scroll", (e) => {
+  if (window.scrollY > 200) {
+    scrollHeader.classList.add("scroll-header");
+    scrollHeader.style.position = "fixed";
+  } else {
+    scrollHeader.classList.remove("scroll-header");
+    scrollHeader.style.position = "absolute";
+  }
+});
+
+// Search
+
+const openSearch = document.querySelector("#openSearch");
+const searchOverlay = document.querySelector(".searchOverlay");
+const searchInput = document.querySelector(".searchInput");
+const closeSearchInput = document.querySelector("#close--searchInput");
+const inputSearch = document.querySelector("#inputSearch");
+const searchInputValue = document.querySelector("#searchInputValue");
+const message = document.querySelector(".message");
+
+openSearch.addEventListener("click", () => {
+  searchOverlay.classList.remove("hidden");
+  searchInput.classList.remove("hidden");
+  closeSearchInput.style.display = "flex";
+});
+closeSearchInput.addEventListener("click", () => {
+  searchOverlay.classList.add("hidden");
+  searchInput.classList.add("hidden");
+  closeSearchInput.style.display = "none";
+});
+
+inputSearch.addEventListener("click", () => {
+  if (searchInputValue.value.length > 0) {
+    message.textContent = "";
+  } else {
+    message.textContent = `Iltimos, qidirilayotgan narsani kiriting...`;
+    setTimeout(() => {
+      message.textContent = "";
+    }, 4000);
+  }
 });
